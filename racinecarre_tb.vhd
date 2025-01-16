c8c2f97a13dfc00e0fe671f1b1086c7908541b55
fabquero <fabien.quero@gmail.com>
2025-01-16 12:05:46 +0100

    update: erreur start dans le testbench

--- racinecarre_tb.vhd
+++ racinecarre_tb.vhd
@@ -5,19 +5,20 @@ entity racinecarre_tb is
 end racinecarre_tb;
 
 architecture behavior of racinecarre_tb is
-    constant nb: integer := 32;
-    constant clock_period: time := 100 ns;
+    constant nb: natural := 32;
+    -- periode d'horloge de 10ns typique du Nios II standard
+    constant clock_period: time := 10 ns;
 
     signal clk : std_logic := '0';
     signal start : std_logic := '0';
     signal rst : std_logic := '0';
-    signal A : std_logic_vector(nb-1 downto 0) := (9=>'1',others => '0');
-    signal Result : std_logic_vector(nb-1 downto 0);
+    signal A : std_logic_vector(2*nb-1 downto 0) := (9=>'1',others => '0');
+    signal Result : std_logic_vector((nb-1) downto 0);
     signal done : std_logic;
     
     begin
-     racinecarre : entity work.racinecarre(arch)
-        generic map(N => nb)
+     racinecarree : entity work.racinecarree(arch)
+        generic map(n_bits => nb)
         port map (
             clk => clk,
             rst => rst,
@@ -27,7 +28,13 @@ architecture behavior of racinecarre_tb is
             data_out => Result
         );
 
-    rst <= '1';
+    reset_process: process
+    begin
+        rst <= '1';
+        wait for clock_period;
+        rst <= '0';
+        wait;
+    end process;
 
     clock_process: process begin
         clk <= '0';
@@ -36,15 +43,16 @@ architecture behavior of racinecarre_tb is
         wait for clock_period/2;
     end process;
 
-    process begin
+    simple_test: process begin
         start <= '0';
         wait for clock_period*2;
         start <= '1';
-        if done = '1' then
-            -- test qu'on a bien 2^5 = 32
-            assert Result = "00000000000000000000000000010000" report "Erreur, ce n'est pas la valeur 2^5" severity error;
-            assert false report "Test: OK" severity failure;
-        end if;
+        wait for clock_period*2;
+       if done = '1' then
+            --test qu'on a bien 2^5 = 32
+           assert Result = "00000000000000000000000000010000" report "Erreur, ce n'est pas la valeur 2^5" severity error;
+           report "Test Done." severity failure ;
+       end if;
     end process;
 
 
