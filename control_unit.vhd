a9ac79f86c203389f23ba034fd987839ba58f5fe
François Deleu <francoisdeleu4@gmail.com>
2025-01-16 09:27:17 +0100

    swapped cu adder for shift register

--- control_unit.vhd
+++ control_unit.vhd
@@ -19,27 +19,27 @@ architecture structural of control_unit is
     component data_register
 		generic(n_bits: natural);
 		port(
-			clk, rst, ena: in  std_logic;
-			D            : in  std_logic_vector(n_bits - 1 downto 0);
-			Q            : out std_logic_vector(n_bits - 1 downto 0)
+			clk, rst, ena: in std_logic;
+			D: in  std_logic_vector(n_bits - 1 downto 0);
+			Q: out std_logic_vector(n_bits - 1 downto 0)
 		);
 	end component;
 
     -- 00: init; 01: computing; 10: done
     signal state, n_state: std_logic_vector(1 downto 0);
     
-    -- counter for computation state
-    component decrementing_counter
-        generic(ctr_bits, ctr_init: natural);
+    -- shift register for computation state
+    component shift_register
+        generic(n_bits: natural);
         port(
-            clk, rst: in std_logic;
-            Q: out std_logic_vector(ctr_bits - 1 downto 0)
+            clk, rst, ena: in std_logic;
+            Q: out std_logic;
+            D: in  std_logic_vector(n_bits - 1 downto 0)
         );
     end component;
 
-    -- counter signals
-    signal ctr_rst: std_logic;
-    signal ctr: std_logic_vector(clog2(n_bits) - 1 downto 0);
+    signal sr_out, sr_rst, sr_ena: std_logic;
+    signal sr_in:  std_logic_vector(n_bits - 1 downto 0);
 begin
     -- sequential state update
     state_reg : data_register
@@ -50,17 +50,15 @@ begin
     );
 
     -- computation state counter
-    ctr_rst <= '1' when not (state ="01") or (rst = '1') else '0';
+    sr_rst <= '1' when (state ="10") or (rst = '1') else '0';
+    sr_in  <= (n_bits - 1 downto 1 => '0') & '1';
+    sr_ena <= '1' when state = "00" else '0';
 
-    counter : decrementing_counter
-    generic map(
-        ctr_bits => clog2(n_bits),
-        ctr_init => n_bits - 1
-    )
+    sr : shift_register
+    generic map(n_bits => n_bits)
     port map(
-        clk => clk,
-        rst => ctr_rst,
-        Q => ctr
+        clk => clk, rst => sr_rst, ena => sr_ena,
+        D => sr_in, Q => sr_out
     );
 
     -- next state computation
@@ -71,10 +69,10 @@ begin
         else
             n_state <= state;
             case state is
-                when "00" =>     -- start computation when start
+                when "00" =>
                     n_state <= "01";
-                when "01" =>     -- stop computation at the end of the counter
-                    if ctr = "0" then
+                when "01" =>
+                    if sr_out = '1' then
                         n_state <= "10";
                     end if;
                 when others =>
